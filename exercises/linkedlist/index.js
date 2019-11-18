70a28d515e5671695bf73fde616736818905285b
Víctor Rosa <vicrosa25@gmail.com>
2019-11-18 09:08:04 +0100

    Queue done

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,28 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    let node = new Node(data);
+    if (this.head) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      this.head = node;
+    }
+    return this;
+  }
+}
 
 module.exports = { Node, LinkedList };
