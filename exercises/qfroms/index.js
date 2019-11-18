3d59cd6d96e2174c186f6e982315430748d0b24d
Víctor Rosa <vicrosa25@gmail.com>
2019-11-18 10:55:26 +0100

    Queue from Stack exercise done

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,40 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.in = new Stack();
+    this.out = new Stack();
+  }
+
+  add(record) {
+    this.in.push(record);
+  }
+
+  remove() {
+    // Reverse the "in" stack and fill the out stack
+    while (this.in.peek()) {
+      this.out.push(this.in.pop());
+    }
+    return this.out.pop();
+  }
+
+  peek() {
+    // Reverse the "in" stack and fill the out stack
+    while (this.in.peek()) {
+      this.out.push(this.in.pop());
+    }
+    // Made a reference to correct value
+    const result = this.out.peek();
+
+    // Get back to the original state
+    while (this.out.peek()) {
+      this.in.push(this.out.pop());
+    }
+    return result;
+  }
+}
 
 module.exports = Queue;
